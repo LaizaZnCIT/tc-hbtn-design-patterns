7ef12561c95b6e260839b76531abf0f861efda94
LaizaZnCIT <laiza@ciandt.com>
2022-08-19 14:30:42 -0300

    Padrões de projeto task 1

---
+++ 0x00/personagem-builder/Mago.java
@@ -0,0 +1,13 @@
+public class Mago extends Personagem {
+
+    public Mago(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        super(nome, TipoPersonagem.MAGO, inteligencia, forca, vigor, resistencia, destreza);
+        if(inteligencia < forca || inteligencia < destreza)
+            throw new IllegalArgumentException("Atributos invalidos para " + TipoPersonagem.MAGO);
+    }
+
+    @Override
+    public double getDanoAtaque() {
+        return (getInteligencia() * 0.8) + (getForca() * 0.05) + (getDestreza() * 0.05) + (getVigor() * 0.1);
+    }
+}
