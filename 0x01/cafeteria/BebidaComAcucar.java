1329006c521e2c387d0b9a8bf2f5f5f38c106f17
LaizaZnCIT <laiza@ciandt.com>
2022-08-22 11:52:19 -0300

    Padrões de projeto continuação task 1

---
+++ 0x01/cafeteria/BebidaComAcucar.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BebidaComAcucar extends BebidaDecorator {
+
+    public BebidaComAcucar(Bebida bebidaDecorada) {
+        super(bebidaDecorada);
+    }
+
+    @Override
+    public List<String> obterIngredientes() {
+        List<String> list = super.obterIngredientes();
+        list.add("acucar");
+        return list;
+    }
+
+    @Override
+    public double obterPreco() {
+        return 1.9 + super.obterPreco();
+    }
+}
