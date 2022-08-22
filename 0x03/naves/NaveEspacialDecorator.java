d4c94c87225e54d85a7fe39e6c2a5d9bb99f6499
LaizaZnCIT <laiza@ciandt.com>
2022-08-22 15:46:31 -0300

    Padrões de projeto na prática parte 2 task 0

--- 0x03/naves/NaveEspacialDecorator.java
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -1,14 +1,14 @@
 public class NaveEspacialDecorator extends NaveEspacial {
-    private NaveEspacial naveEspacial;
+    private NaveEspacial naveDecorada;
 
-    public NaveEspacialDecorator(NaveEspacial naveEspacial) {
-        super(naveEspacial.getSaude(), naveEspacial.getAtaque());
+    public NaveEspacialDecorator(NaveEspacial naveDecorada) {
+        super(naveDecorada.getSaude(), naveDecorada.getAtaque());
     }
-
+    @Override
     public int getSaude(){
         return super.getSaude();
     }
-
+    @Override
     public int getAtaque(){
         return super.getAtaque();
     }
